61a7a445552d9046acb8eb48adafbc0c2172c2cc
Diksha <diksha.2895@gmail.com>
2015-10-23 21:58:35 +0530

    changes to the database presentation in admin

--- bins/admin.py
+++ bins/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from bins.models import *
 
+class Display_Definition(admin.ModelAdmin):
+    list_display = ('location_id', 'bin_id', 'fill_level', 'humidity', 'temperature')
+
 # Register your models here.
-admin.site.register(bin_info)
+admin.site.register(bin_info, Display_Definition)
--- bins/models.py
+++ bins/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class bin_info(models.Model):
-	location_id = models.CharField(max_length=60,blank=False,unique=False)
+	location_id = models.CharField(max_length=60,unique=False)
 	bin_id = models.CharField(max_length=30,blank=True,null=True,unique=False)
-	fill_level = models.IntegerField(blank=False,unique=False)
-	humidity = models.IntegerField(blank=False,unique=False)
-	temperature = models.IntegerField(blank=False,unique=False)
+	fill_level = models.IntegerField(unique=False)
+	humidity = models.IntegerField(unique=False)
+	temperature = models.IntegerField(unique=False)
 
 	def __unicode__(self):
     		return u'%s %s %d %d %d' % (self.location_id, self.bin_id, self.fill_level, self.humidity, self.temperature)
