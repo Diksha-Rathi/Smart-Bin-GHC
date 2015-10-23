eb5f2b9464b2fd158dba5ba7d85b3f0b5c7bc3e7
Diksha <diksha.2895@gmail.com>
2015-10-24 01:28:01 +0530

    completed bin display on tab 1

--- bins/views.py
+++ bins/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from bins.models import *
 
 # Create your views here.
 def bins(request):
-	result = bin_info.objects.filter(location_id='Location 1')
-        return render(request, 'bins.html',{'result': result})
+	result1 = bin_info.objects.filter(fill_level__range=(71, 100))
+	result2 = bin_info.objects.filter(fill_level__range=(51, 70))
+	result3 = bin_info.objects.filter(fill_level__range=(0, 50))
+	return render(request, 'bins.html',{'result1': result1, 'result2': result2, 'result3': result3})
