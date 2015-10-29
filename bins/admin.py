472fc3293bb64389097434dff40e28ab1cfe51c9
tanvi-surana <tanvisurana20@gmail.com>
2015-10-29 21:15:15 +0530

    bin db edit diksha-code

--- bins/admin.py
+++ bins/admin.py
@@ -1,20 +1,20 @@
-# from django.contrib import admin
-# from bins.models import *
-
-
-# class Display_Definition1(admin.ModelAdmin):
-# 	  list_display = ('location_id', 'longitude', 'latitude')
-
-# class Display_Definition2(admin.ModelAdmin):
-# 	  list_display = ('location_id', 'bin_id', 'date', 'fill_level', 'humidity', 'temperature')
-
-# # Register your models here.
-# admin.site.register(bin_record, Display_Definition1)
-# admin.site.register(bin_updates, Display_Definition2)
-
-
 from django.contrib import admin
-from binDetails.models import *
+from bins.models import *
+
+
+class Display_Definition1(admin.ModelAdmin):
+	  list_display = ('location_id', 'longitude', 'latitude')
+
+class Display_Definition2(admin.ModelAdmin):
+	  list_display = ('location_id', 'bin_id', 'date', 'fill_level', 'humidity', 'temperature')
+
 # Register your models here.
-admin.site.register(binAdded)
-admin.site.register(binDescription)
+admin.site.register(bin_record, Display_Definition1)
+admin.site.register(bin_updates, Display_Definition2)
+
+
+# from django.contrib import admin
+# from binDetails.models import *
+# # Register your models here.
+# admin.site.register(binAdded)
+# admin.site.register(binDescription)
--- bins/models.py
+++ bins/models.py
@@ -1,53 +1,53 @@
-# from django.db import models
-# #from datetime import datetime
-# #from datetime import date
-
-# # Create your models here.
-# class bin_record(models.Model):
-# 	location_id = models.CharField(max_length=60)
-# 	longitude=models.DecimalField(max_digits=15,decimal_places=10)
-# 	latitude=models.DecimalField(max_digits=15,decimal_places=10)
-
-# 	class Meta:
-# 			ordering = ['id']
-
-# class bin_updates(models.Model):
-# 	location_id = models.ForeignKey(bin_record)
-# 	bin_id = models.CharField(max_length=30,blank=True,null=True)
-# 	#date_id = models.DateField(_("Date"), default=date.today)
-# 	date = models.DateField( auto_now_add=True)
-# 	fill_level = models.IntegerField()
-# 	humidity = models.IntegerField()
-# 	temperature = models.IntegerField()
-
-# 	class Meta:
-# 			ordering = ['date']
-
-
-
-
 from django.db import models
+#from datetime import datetime
+#from datetime import date
 
 # Create your models here.
+class bin_record(models.Model):
+	location_id = models.CharField(max_length=60)
+	longitude=models.DecimalField(max_digits=15,decimal_places=10)
+	latitude=models.DecimalField(max_digits=15,decimal_places=10)
 
-class binAdded(models.Model):
-	bin_id=models.CharField(max_length=20,blank=False,unique=True)
-	date_added=models.DateField(auto_now_add=True)
-	longitude=models.DecimalField(max_digits=20,decimal_places=13)
-	latitude=models.DecimalField(max_digits=20,decimal_places=13)
-
-	def __str__(self):
-		return self.bin_id
 	class Meta:
-		verbose_name = 'bin'
-		verbose_name_plural='bins'
+			ordering = ['id']
 
-class binDescription(models.Model):
-	bin_id=models.ForeignKey(binAdded)
-	current_date=models.DateField()
-	fill_level=models.DecimalField(max_digits=5, decimal_places=3)
-	temperature=models.IntegerField()
+class bin_updates(models.Model):
+	location_id = models.ForeignKey(bin_record)
+	bin_id = models.CharField(max_length=30,blank=True,null=True)
+	#date_id = models.DateField(_("Date"), default=date.today)
+	date = models.DateField( auto_now_add=True)
+	fill_level = models.IntegerField()
 	humidity = models.IntegerField()
+	temperature = models.IntegerField()
 
-	def __str__(self):
-		return self.bin_id.bin_id
+	class Meta:
+			ordering = ['date']
+
+
+
+
+# from django.db import models
+
+# # Create your models here.
+
+# class binAdded(models.Model):
+# 	bin_id=models.CharField(max_length=20,blank=False,unique=True)
+# 	date_added=models.DateField(auto_now_add=True)
+# 	longitude=models.DecimalField(max_digits=20,decimal_places=13)
+# 	latitude=models.DecimalField(max_digits=20,decimal_places=13)
+
+# 	def __str__(self):
+# 		return self.bin_id
+# 	class Meta:
+# 		verbose_name = 'bin'
+# 		verbose_name_plural='bins'
+
+# class bin_updates(models.Model):
+# 	bin_id=models.ForeignKey(binAdded)
+# 	current_date=models.DateField()
+# 	fill_level=models.DecimalField(max_digits=5, decimal_places=3)
+# 	temperature=models.IntegerField()
+# 	humidity = models.IntegerField()
+
+# 	def __str__(self):
+# 		return self.bin_id.bin_id
