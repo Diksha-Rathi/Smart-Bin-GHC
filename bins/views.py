fcfcb272d77a31ee8ace091a85de1ea360cc651e
Tanvi Surana <tanvisurana20@gmail.com>
2015-11-01 13:55:00 +0530

    csv report download

--- bins/views.py
+++ bins/views.py
@@ -7,6 +7,12 @@ from django.contrib.auth.decorators import login_required
 from django.core import serializers
 from django.http import HttpResponse
 from datetime import date
+from django.template.loader import get_template
+from django.template import RequestContext
+#from weasyprint import HTML, CSS
+#from django.http import HttpResponse
+from django.template import loader, Context
+import djqscsv
 
 # Create your views here.
 @login_required
@@ -42,3 +48,50 @@ def my_json_view2(request):
 	bin_update = bin_updates.objects.all()
 	data = serializers.serialize("json",bin_update)
 	return HttpResponse(data, content_type="application/json")
+	
+###----------------------------------pip install django-queryset-csv-------------------###
+@login_required
+def get_csv(request):
+    qs = bin_updates.objects.all()
+    return djqscsv.render_to_csv_response(qs)
+
+
+# @login_required
+# def get_report(request):
+#     html_template = get_template('bins.html')
+#     bin_data= bin_record.objects.all()
+#     rendered_html = html_template.render(RequestContext(request, {'bin':bin_data})).encode(encoding="UTF-8")
+#     pdf_file = HTML(string=rendered_html).write_pdf(stylesheets=[CSS(settings.STATIC_ROOT +  'css/bootstrap.css')])
+#     http_response = HttpResponse(pdf_file, content_type='application/pdf')
+#     http_response['Content-Disposition'] = 'filename="report.pdf"'
+#     return response
+
+# def get_report(request):
+#     template = get_template("bins.html")
+#     context = {
+#      'bin':bin_record.objects.all()
+#      }
+#     html = template.render(RequestContext(request, context))
+#     response = HttpResponse(content_type="application/pdf")
+#     weasyprint.HTML(string=html, base_url=request.build_absolute_uri(), url_fetcher=request).write_pdf(response)
+#     return response
+
+
+# def some_view(request):
+#     # Create the HttpResponse object with the appropriate CSV header.
+#     response = HttpResponse(content_type='text/csv')
+#     response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+
+#     # The data is hard-coded here, but you could load it from a database or
+#     # some other source.
+#     csv_data = (
+#         ('First row', 'Foo', 'Bar', 'Baz'),
+#         ('Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"),
+#     )
+
+#     t = loader.get_template('my_template_name.txt')
+#     c = Context({
+#         'data': csv_data,
+#     })
+#     response.write(t.render(c))
+#     return response	
